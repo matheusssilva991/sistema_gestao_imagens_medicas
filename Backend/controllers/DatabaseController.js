63b5eb3296f38b0d9ffa2bc00511e08e46a22332
matheusssilva991 <matheus.ssilva991@gmail.com>
2023-06-02 19:08:48 -0300

    Att Documentação + entidade database

--- Backend/controllers/DatabaseController.js
+++ Backend/controllers/DatabaseController.js
@@ -1,7 +1,5 @@
 const DatabaseService = require("../services/DatabaseService");
 const ImageTypeService = require("../services/ImageTypeService");
-const ImageProcessingService = require("../services/imageProcessingService");
-const fs = require("fs");
 
 class DatabaseController {
 
@@ -19,84 +17,6 @@ class DatabaseController {
         if (databases[0]){
             res.status(200).json(databases[0]);
             return
-        } else {
-            res.status(404).json({ err: "Banco de dados não encontrado" });
-            return;
-        }
-    }
-
-    async getImages (req, res) {
-        const databases = await DatabaseService.find();
-        let images = []
-
-        for (let database of databases) {
-            let index = 0;
-
-            for (let image of database.images.slice(0, 20)) {
-
-                if (image.imagePath.includes("dcm")){
-                    let newPath = `./assets/images/image_${index}_${database.name}.png`;
-
-                    // Converte imagem Dicom em PNG
-                    ImageProcessingService.convertDicomPNGSync(image.imagePath, newPath);
-                    image.imagePath = newPath;
-
-                    // Redimensiona a imagem
-                    newPath = newPath.replace(".png", "_resized.png");
-                    ImageProcessingService.resizeSync(image.imagePath, { width: 64, height: 64 }, newPath);
-
-                    image.imagePath = newPath;
-
-                } else {
-                    let newPath = `./assets/images/image_${index}_${database.name}_resized.png`;
-
-                    ImageProcessingService.resizeSync(image.imagePath, { width: 64, height: 64 }, newPath);
-                    image.imagePath = newPath;
-                }
-
-                images.push(image);
-                index++;
-            }
-        }
-
-        res.status(200).json(images);
-        return
-    }
-
-    async getDatabaseImages (req, res) {
-        const databaseName = req.params.databaseName;
-        const database = await DatabaseService.find({ name:databaseName });
-        let images = []
-
-        if (database[0]) {
-            for (let image of database[0].images.slice(0, 20)) {
-
-                if (image.imagePath.includes("dcm")){
-                    let newPath = `./assets/images/image_${index}_${database.name}.png`;
-
-                    // Converte imagem Dicom em PNG
-                    ImageProcessingService.convertDicomPNGSync(image.imagePath, newPath);
-                    image.imagePath = newPath;
-
-                    // Redimensiona a imagem
-                    newPath = newPath.replace(".png", "_resized.png");
-                    ImageProcessingService.resizeSync(image.imagePath, { width: 64, height: 64 }, newPath);
-
-                    image.imagePath = newPath;
-                } else {
-                    let newPath = `./assets/images/image_${index}_${database.name}_resized.png`;
-
-                    ImageProcessingService.resizeSync(image.imagePath, { width: 64, height: 64 }, newPath);
-                    image.imagePath = newPath;
-                }
-
-                images.push(image);
-                index++;
-            }
-
-            res.status(200).json(images);
-            return
-
         } else {
             res.status(404).json({ err: "Banco de dados não encontrado" });
             return;
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const userRoutes = require("./userRoutes");
 const authRoutes = require("./authRoutes");
 const databaseRoutes = require("./databaseRoutes");
-const imageRoutes = require("./imageRoutes");
 const imageTypeRoutes = require("./imageTypeRoutes");
 const solicitationRoutes = require("./solicitationRoutes");
 
 router.use("/", userRoutes);
 router.use("/", authRoutes);
 router.use("/", databaseRoutes);
-router.use("/", imageRoutes);
 router.use("/", imageTypeRoutes);
 router.use("/", solicitationRoutes);
 
