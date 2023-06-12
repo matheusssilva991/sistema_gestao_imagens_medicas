419060f5af0bf5bc52c2269ee5590caee74a1258
Cleber Junior <cleberspjr@gmail.com>
2023-06-12 18:48:59 -0300

    atualização

--- Frontend/src/views/ImagesView.vue
+++ Frontend/src/views/ImagesView.vue
@@ -14,22 +14,46 @@
                     </div>
                 </div>
             </div>
-
             <div class="imagens mt-4">
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
-                <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="" onclick="openModal()"/></li>
+               <template v-for="(imagem, index) in imagensPagina" :key="index">
+                 <li><img class="mamografia.jpg" src="../assets/mamografia.jpg" :alt="imagem.alt" @click="openModal('mamografia.jpg')"/></li>
+               </template>
             </div>
-      </div>
 
-    </div>
+            <div class="pagination">
+                <div class="pagination-buttons">
+                    <button @click="paginaAnterior" :disabled="paginaAtual === 1" class="pagination-button">Anterior</button>
+                    <button @click="proximaPagina" :disabled="!existemMaisPaginas" class="pagination-button">Próxima</button>
+                </div>
+            </div>
+            <div class="modal" v-if="selectedImage">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h2 class="modal-title">Visualização</h2>
+                  <button class="modal-close" @click="closeModal">&#10006;</button>
+                </div>
+                 <div class="modal-body">
+                   <div class="modal-image-container">
+                      <img class="mamografia.jpg" src="../assets/mamografia.jpg" alt="image.alt">
+                   </div>
+                   <div class="modal-info">
+                   <label for="info1">ID Paciente:</label>
+                   <input type="text" id="info1" :value="imagensBackend[0].idPaciente" disabled>
+                   <label for="info2">Orientação:</label>
+                   <input type="text" id="info2" v-model="imagensBackend[0].orientacao" disabled>
+                   <label for="info3">Qualidade:</label>
+                   <input type="text" id="info3" v-model="imagensBackend[0].qualidade" disabled>
+                   <label for="info4">Patologia:</label>
+                   <input type="text" id="info4" v-model="imagensBackend[0].patologia" disabled>
+                   </div>
+               </div>
+
+               </div>
+             </div>
+           </div>
+         </div>
 </template>
-  
+
 <script>
 
 import InputComp from '../components/InputComp.vue'
@@ -40,36 +64,84 @@ export default {
     },
     data() {
         return {
+            selectedImage: null,
+            info1: '',
+            info2: '',
+            info3: '',
+            info4: '',
+            imagensBackend: [], // Array para armazenar as imagens obtidas do backend
+            imagensPorPagina: 8,
+            paginaAtual: 1
         };
+
     },
     computed: {
+    existemMaisPaginas() {
+      return this.paginaAtual < this.totalPaginas;
     },
-    methods: {
-        openModal(solicitation) {
-            this.selectedSolicitation = solicitation;
-
-            if (solicitation.type == 'Novo Banco de imagens'){
-                this.showDatabaseModal = true;
-            }
-            else{
-                this.showUserModal = true;  
-            }
-
-        },
-        closeModal() {
-            this.selectedSolicitation = null;
-            this.showUserModal = false;
-            this.showDatabaseModal = false;
-        },
+    totalPaginas() {
+      return Math.ceil(this.imagensBackend.length / this.imagensPorPagina);
+    },
+    imagensPagina() {
+      const inicio = (this.paginaAtual - 1) * this.imagensPorPagina;
+      const fim = inicio + this.imagensPorPagina;
+      return this.imagensBackend.slice(inicio, fim);
     }
+  },
+  methods: {
+    openModal(image) {
+      this.selectedImage = image;
+    },
+    closeModal() {
+      this.selectedImage = null;
+      this.info1 = '';
+      this.info2 = '';
+      this.info3 = '';
+      this.info4 = '';
+    },
+    paginaAnterior() {
+      if (this.paginaAtual > 1) {
+        this.paginaAtual--;
+      }
+    },
+    proximaPagina() {
+      if (this.paginaAtual < this.totalPaginas) {
+        this.paginaAtual++;
+      }
+    }
+  },
+  created() {
+/*teste*/
+    this.imagensBackend = [
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 1', idPaciente: '123', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 2' , idPaciente: '124', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 3' , idPaciente: '125', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 4' , idPaciente: '126', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 5' , idPaciente: '127', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 6' , idPaciente: '128', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 7', idPaciente: '129', orientacao: '...', qualidade: '...', patologia: '...'  },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 8' , idPaciente: '130', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 9' , idPaciente: '131', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 10' , idPaciente: '132', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 11' , idPaciente: '133', orientacao: '...', qualidade: '...', patologia: '...' },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 12', idPaciente: '134', orientacao: '...', qualidade: '...', patologia: '...'  },
+      { src: '../assets/mamografia.jpg', alt: 'Imagem 13', idPaciente: '135', orientacao: '...', qualidade: '...', patologia: '...'  },
+
+
+    ];
+  }
 };
+
 </script>
-  
+
 <style scoped>
-  
+
   * {
     color: #73bf8e;
     font-weight: 100;
+}
+.container{
+    background-color: #f2f2f2;
 }
 .sidebar {
     font-family: 'Montserrat', sans-serif;
@@ -77,7 +149,7 @@ export default {
     padding: 5%;
     padding-top: 2.5%;
     margin: 0 auto; /* Adicionado para centralizar horizontalmente */
-    height: 70vh;
+    height: 80vh;
     width: 100%;
     border-radius: 25px;
     box-shadow: 0 4px 7px rgba(0, 0, 0, 0.613);
@@ -120,7 +192,7 @@ export default {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  align-content: space-between;
+  align-content: center;
 }
 
 .imagens img {
@@ -132,5 +204,231 @@ export default {
 .imagens li {
   list-style-type: none;
 }
+
+.pagination {
+  position: center;
+  bottom: 6px;
+  right: 10px;
+  margin-top: 12px;
+  margin-left: 80%
+}
+
+.pagination-buttons {
+  display: flex;
+  gap: 5px;
+}
+
+.pagination-button {
+  padding: 5px 10px;
+  font-size: 14px;
+}
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: #ffffff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 7px rgba(0, 0, 0, 0.613);
+  text-align: center;
+  width: 40%;
+  max-width: 800px;
+  max-height: 90%;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.modal-title {
+  font-size: 20px;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.modal-body {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.modal-image-container {
+  flex: 1;
+  text-align: left;
+}
+
+.modal-image {
+  max-width: 100%;
+  max-height: 400px;
+  width: auto;
+}
+
+.modal-info {
+  flex: 1;
+  margin-left: 20px;
+  text-align: left;
+  
+}
+
+.modal-info label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+.modal-info input {
+  width: 100%;
+  padding: 5px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  background-color: #f2f2f2;
+  border: 0.5px solid #f2f2f2;
+}
+
+.modal-close {
+  padding: 5px 10px;
+  background-color: #73bf8e;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+
+
+@media (max-width: 576px) {
+    .container{
+    background-color: #f2f2f2;
+}
+  .sidebar {
+    height: auto;
+  }
+
+  .sidebar-title {
+    font-size: 20px;
+  }
+
+  #filter {
+    width: 80%;
+    margin-top: 10px;
+  }
+
+  .imagens {
+    justify-content: center;
+
+  }
+
+  .imagens li {
+    width: 90%;
+    margin: 10px;
+  }
+
+  .pagination{
+    margin: auto;
+  }
+
+    .modal-content {
+    width: 90%;
+  }
+
+   .modal-body {
+    flex-direction: column;
+  } 
+
+  .modal-image-container{
+    flex: 1;
+    text-align: center;
+  }
+  .modal-info {
+    margin: 10px 0;
+  }
+
+  .modal-image {
+    max-height: 200px;
+  }
+}
+
+
+@media (min-width: 340px) and (max-width: 576px){
+    .sidebar{
+        height: auto;
+    }
+    .pagination{
+      bottom: 1px;
+    }
+} 
+ @media (min-width:577px) and (max-width: 735px) {
+
+    .modal-content {
+    width: 90%;
+  }
+
+  .modal-body {
+    flex-direction: column;
+  }
+
+  .modal-image-container{
+    flex: 1;
+    text-align: center;
+  }
+  .modal-info {
+    margin: 10px 0;
+  }
+
+  .modal-image {
+    max-height: 200px;
+  }
+
+  .sidebar-title {
+    margin-top: -5%;
+    text-align: center;
+  }
+  .sidebar-header{
+    justify-content: center;
+  }
+
+  .sidebar{
+    height: auto;
+  }
+  .pagination{
+    margin-left: auto;
+  }
+} 
+
+ @media (min-width: 736px) and (max-width: 1000px) {
+  .container{
+    background-color: #f2f2f2;
+  }
+  .imagens li {
+    width: 40%;
+    margin: 10px;
+  }
+  .sidebar{
+    height: auto;
+  }
+  .pagination{
+    margin:auto;
+  }
+} 
+@media (min-width: 1001px) and (max-width: 1220px) {
+  .imagens li {
+    width: 30%;
+    margin: 10px;
+  }
+  .sidebar{
+    height: auto;
+  }
+} 
 
 </style>
